16356c5eeb6d42be040b1e2b03214e375dc69a56
Oscar <101271985+bkoscar@users.noreply.github.com>
2022-06-11 11:46:53 -0500

    Problem3.1

--- problem3.py
+++ problem3.py
@@ -7,6 +7,15 @@ a//b . The second line should contain the result of float division, a/b .
 """
 
 def division_numbers(a:int, b:int):
+    """ This function calculates the integers division and the float division.
+
+    Args:
+        a (int): integer number
+        b (int): integer number
+
+    Raises:
+        ValueError: If the value of integer b is zero, then rise value error.
+    """
     if b ==0:
         raise ValueError("The number b can't be zero")
     else:
