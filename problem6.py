eac6b8b80c1ccb76d2aec0b1ff17c5f6c66e6355
Oscar <101271985+bkoscar@users.noreply.github.com>
2022-06-16 10:27:58 -0500

    Problem 6.1

--- problem6.py
+++ problem6.py
@@ -1,15 +1,16 @@
 # Print Function
 
-def blank_spaces(n:int):
-    """This function given a integer number n print all numbers concatenated from 1 to n_
+def blank_spaces(n: int):
+    """This function given a integer number n print all
+    numbers concatenated from 1 to n
 
     Args:
         n (int): integer number
     """
-    for i in range(1,n+1,1):
+    for i in range(1, n+1, 1):
         print(i, end='')
-        
-        
+
+
 if __name__ == '__main__':
     n = int(input())
     blank_spaces(n)
