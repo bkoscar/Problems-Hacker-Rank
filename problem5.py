6c180d0f5874b085eb4252ba422ed98f89e84c1c
Oscar <101271985+bkoscar@users.noreply.github.com>
2022-06-16 09:50:19 -0500

    Problem 5

---
+++ problem5.py
@@ -0,0 +1,25 @@
+# Leap year problem
+
+def is_leap(year:int):
+    """This function  given a year return when is a year leap
+
+    Args:
+        year (int): Year
+
+    Returns:
+        Bolean: When a year is a year leap
+    """
+    leap = True
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year% 400 == 0:
+                return leap 
+            return not leap
+        return leap
+    else:
+        return not leap 
+
+
+if __name__ == '__main__':
+    year = int(input( ))
+    print(is_leap(year))
